051d11af840f6328c5fd52919336db9552203cb4
SarswatiPatil <dipakalagate1991@gmail.com>
2019-12-11 15:35:58 +0530

    Added test code to ensure Number of records matches

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -12,12 +12,13 @@ public class CensusAnalyserTest
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
     private static final String INDIA_STATE_CSV_FILE_PATH="/home/admin1/Desktop/CensusAnalyser/src/test/resources/IndiaStateCode.csv";
 
+    CensusAnalyser censusAnalyser = new CensusAnalyser();
+
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords()
     {
         try
         {
-            CensusAnalyser censusAnalyser = new CensusAnalyser();
             int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
             Assert.assertEquals(29,numOfRecords);
         }
@@ -32,7 +33,6 @@ public class CensusAnalyserTest
     {
         try
         {
-            CensusAnalyser censusAnalyser = new CensusAnalyser();
             ExpectedException exceptionRule = ExpectedException.none();
             exceptionRule.expect(CSVBuilderException.class);
             censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
@@ -46,7 +46,6 @@ public class CensusAnalyserTest
     @Test
     public void givenIndianStateCSV_ShouldReturnExactCount()
     {
-        CensusAnalyser censusAnalyser=new CensusAnalyser();
         try
         {
             int numOfRecords=censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
@@ -62,12 +61,10 @@ public class CensusAnalyserTest
     @Test
     public void givenIndianCensusData_WhenSortedOnState_ShouldReturnSortedResult()
     {
-        CensusAnalyser censusAnalyser=new CensusAnalyser();
-        String sortedCensusData = null;
         try
         {
             censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
-            sortedCensusData = censusAnalyser.giveStateWiseSortedCensusData();
+            String sortedCensusData = censusAnalyser.giveStateWiseSortedCensusData();
             IndiaCensusCSV[] censusCSVS=new Gson().fromJson(sortedCensusData,IndiaCensusCSV[].class);
             Assert.assertEquals("Andhra Pradesh",censusCSVS[0].state);
         }
@@ -75,5 +72,20 @@ public class CensusAnalyserTest
         {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    public void givenIndianStateCodeCSVFileReturnsCorrectRecords()
+    {
+        try
+        {
+            int numOfRecords=censusAnalyser.loadIndianStateCode(INDIA_STATE_CSV_FILE_PATH);
+            Assert.assertEquals(37,numOfRecords);
+        }
+        catch (CSVBuilderException e)
+        {
+            e.printStackTrace();
+        }
+
     }
 }
