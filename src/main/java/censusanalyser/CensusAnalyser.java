70001d3f82dc1fdb92a12d5b82a07becfba3d588
SarswatiPatil <dipakalagate1991@gmail.com>
2019-12-11 18:46:59 +0530

    Added code to sort Census Data by field using Generic Sort Method

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -3,9 +3,11 @@ import com.bridgelabz.CSVBuilderException;
 import com.bridgelabz.CSVBuilderFactory;
 import com.bridgelabz.ICVBuilder;
 import com.google.gson.Gson;
+import org.apache.commons.collections.map.HashedMap;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
@@ -15,10 +17,16 @@ import java.util.stream.StreamSupport;
 public class CensusAnalyser
 {
     Map<String,IndiaCensusDAO> censusStateMap = null;
+    Map<StateCensusFieldName,Comparator<IndiaCensusDAO>> fieldNameComparatorMap=null;
 
     public CensusAnalyser()
     {
         this.censusStateMap = new HashMap<>();
+        this.fieldNameComparatorMap=new HashedMap();
+        this.fieldNameComparatorMap.put(StateCensusFieldName.State,Comparator.comparing(censusField->censusField.state));
+        this.fieldNameComparatorMap.put(StateCensusFieldName.Population,Comparator.comparing(censusField->censusField.population));
+        this.fieldNameComparatorMap.put(StateCensusFieldName.DensityPerSqKm,Comparator.comparing(censusField->censusField.densityInSqKm));
+        this.fieldNameComparatorMap.put(StateCensusFieldName.AreaInSqKm,Comparator.comparing(censusField->censusField.areaInSqKm));
     }
 
     public int loadIndiaCensusData(String csvFilePath) throws CSVBuilderException
@@ -29,6 +37,7 @@ public class CensusAnalyser
             Iterator<IndiaCensusCSV> csvFileIterator=csvBuilder.getCSVFileIterator(reader,IndiaCensusCSV.class);
             Iterable<IndiaCensusCSV> censusCSVIterable = () -> csvFileIterator;
             StreamSupport.stream(censusCSVIterable.spliterator(), false).forEach(censusCSV -> censusStateMap.put(censusCSV.state,new IndiaCensusDAO(censusCSV)));
+
             return censusStateMap.size();
         }
         catch (IOException | RuntimeException | CSVBuilderException e)
@@ -36,7 +45,6 @@ public class CensusAnalyser
             throw new CSVBuilderException(e.getMessage(),
                     CSVBuilderException.ExceptionType.CENSUS_FILE_PROBLEM);
         }
-
     }
 
     public int loadIndianStateCode(String csvFilePath) throws CSVBuilderException
@@ -63,14 +71,15 @@ public class CensusAnalyser
         }
     }
 
-    public String giveStateWiseSortedCensusData() throws CSVBuilderException
+    public String genericSortMethod(StateCensusFieldName fieldName) throws CSVBuilderException
     {
         if (censusStateMap == null || censusStateMap.size() == 0)
+        {
             throw new CSVBuilderException("No Census Data",
                     CSVBuilderException.ExceptionType.NO_CENSUS_DATA);
-        Comparator<IndiaCensusDAO> csvComparator = Comparator.comparing(census -> census.state);
-        List<IndiaCensusDAO> censusDAOS=censusStateMap.values().stream().collect(Collectors.toList());
-        this.sort(censusDAOS,csvComparator);
+        }
+        List<IndiaCensusDAO> censusDAOS = censusStateMap.values().stream().collect(Collectors.toList());
+        this.sort(censusDAOS, this.fieldNameComparatorMap.get(fieldName));
         String sortedStateCensusJson = new Gson().toJson(censusDAOS);
         return sortedStateCensusJson;
     }
---
+++ src/main/java/censusanalyser/StateCensusFieldName.java
@@ -0,0 +1,6 @@
+package censusanalyser;
+
+public enum StateCensusFieldName
+{
+    State,Population,AreaInSqKm,DensityPerSqKm
+}
